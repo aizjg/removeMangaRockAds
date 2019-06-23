828a4176d01118fe5acaa52176bee7d7e3f0c4e5
Bram De Corte <bram.d.c@hotmail.com>
2019-06-23 19:13:03 +0200

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,7 +2,15 @@
   for this to work you need to inject jquery and then this script in mangarock.
   i do this using the chrome extension javascript injector: https://chrome.google.com/webstore/detail/javascript-injector/djnjegpffahmfpjdlkciiecmeaebghlk
   
+  
+  
+  //inteval remove cause the banners dont come as soon as the page is loaded and every computer/browser is different in speed, 
+  a simple remove every 2 seconds wont make any visable changes on the computer speed
 */
 $(function() {
-    $(".NDm_P").remove();
+    setInterval(function(){
+      $(".NDm_P").remove(); 
+    }, 2000);
+
+    
 });
