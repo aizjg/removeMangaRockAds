3c5a38b1c8e6dbe06dc9beec8392be3ccd99e2ac
Bram De Corte <bram.d.c@hotmail.com>
2019-06-23 19:04:02 +0200

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,8 @@
+/* 
+  for this to work you need to inject jquery and then this script in mangarock.
+  i do this using the chrome extension javascript injector: https://chrome.google.com/webstore/detail/javascript-injector/djnjegpffahmfpjdlkciiecmeaebghlk
+  
+*/
+$(function() {
+    $(".NDm_P").remove();
+});
